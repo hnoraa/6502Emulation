19ed724f09f14b8098d65f58609aa4cd6b7c826b
horetha <horeth@deadlinesolutions.com>
2020-02-26 15:26:46 -0500

    opcode table comments added

--- TestApps/ReadProgram/ReadProgram/cpu.cpp
+++ TestApps/ReadProgram/ReadProgram/cpu.cpp
@@ -154,261 +154,261 @@ void cpu::executeOpCode() {
 		break;
 	case 0x3F:	// RLA abx 7
 		break;
-	case 0x40:
+	case 0x40:	// RTI 6
 		break;
-	case 0x41:
+	case 0x41:	// EOR izx 6
 		break;
-	case 0x42:
+	case 0x42:	// KIL
 		break;
-	case 0x43:
+	case 0x43:	// SRE izx 8
 		break;
-	case 0x44:
+	case 0x44:	// NOP zp 3
 		break;
-	case 0x45:
+	case 0x45:	// EOR zp 3
 		break;
-	case 0x46:
+	case 0x46:	// LSR zp 5
 		break;
-	case 0x47:
+	case 0x47:	// SRE sp 5
 		break;
-	case 0x48:
+	case 0x48:	// PHA 3
 		break;
-	case 0x49:
+	case 0x49:	// EOR imm 2
 		break;
-	case 0x4A:
+	case 0x4A:	// LSR 2
 		break;
-	case 0x4B:
+	case 0x4B:	// ALR imm 2
 		break;
-	case 0x4C:
+	case 0x4C:	// JMP abs 3
 		break;
-	case 0x4D:
+	case 0x4D:	// EOR abs 4
 		break;
-	case 0x4E:
+	case 0x4E:	// LSR abs 6
 		break;
-	case 0x4F:
+	case 0x4F:	// SRE abs 6
 		break;
-	case 0x50:
+	case 0x50:	// BVC rel 2*
 		break;
-	case 0x51:
+	case 0x51:	// EOR izy 5*
 		break;
-	case 0x52:
+	case 0x52:	// KIL
 		break;
-	case 0x53:
+	case 0x53:	// SRE izy 8
 		break;
-	case 0x54:
+	case 0x54:	// NOP zpx 4
 		break;
-	case 0x55:
+	case 0x55:	// EOR zpx 4
 		break;
-	case 0x56:
+	case 0x56:	// LSR zpx 6
 		break;
-	case 0x57:
+	case 0x57:	// SRE zpx 6
 		break;
-	case 0x58:
+	case 0x58:	// CLI 2
 		break;
-	case 0x59:
+	case 0x59:	// EOR aby 4*
 		break;
-	case 0x5A:
+	case 0x5A:	// NOP 2
 		break;
-	case 0x5B:
+	case 0x5B:	// SRE aby 7
 		break;
-	case 0x5C:
+	case 0x5C:	// NOP abx 4*
 		break;
-	case 0x5D:
+	case 0x5D:	// EOR abx 4*
 		break;
-	case 0x5E:
+	case 0x5E:	// LSR abx 7
 		break;
-	case 0x5F:
+	case 0x5F:	// SRE abx 7
 		break;
-	case 0x60:
+	case 0x60:	// RTS 6
 		break;
-	case 0x61:
+	case 0x61:	// ADC izx 6
 		break;
-	case 0x62:
+	case 0x62:	// KIL
 		break;
-	case 0x63:
+	case 0x63:	// RRA izx 8
 		break;
-	case 0x64:
+	case 0x64:	// NOP zp 3
 		break;
-	case 0x65:
+	case 0x65:	// ADC zp 3
 		break;
-	case 0x66:
+	case 0x66:	// ROR zp 5
 		break;
-	case 0x67:
+	case 0x67:	// RRA zp 5
 		break;
-	case 0x68:
+	case 0x68:	// PLA 4
 		break;
-	case 0x69:
+	case 0x69:	// ADC imm 2
 		break;
-	case 0x6A:
+	case 0x6A:	// ROR 2
 		break;
-	case 0x6B:
+	case 0x6B:	// ARR imm 2
 		break;
-	case 0x6C:
+	case 0x6C:	// JMP ind 5
 		break;
-	case 0x6D:
+	case 0x6D:	// ADC abs 4
 		break;
-	case 0x6E:
+	case 0x6E:	// ROR abs 6
 		break;
-	case 0x6F:
+	case 0x6F:	// RRA abs 6
 		break;
-	case 0x70:
+	case 0x70:	// BVS rel 2*
 		break;
-	case 0x71:
+	case 0x71:	// ADC izy 5*
 		break;
-	case 0x72:
+	case 0x72:	// KIL
 		break;
-	case 0x73:
+	case 0x73:	// RRA izy 8
 		break;
-	case 0x74:
+	case 0x74:	// NOP zpx 4
 		break;
-	case 0x75:
+	case 0x75:	// ADC zpx 4
 		break;
-	case 0x76:
+	case 0x76:	// ROR zpx 6
 		break;
-	case 0x77:
+	case 0x77:	// RRA zpx 6
 		break;
-	case 0x78:
+	case 0x78:	// SEI 2
 		break;
-	case 0x79:
+	case 0x79:	// ADC aby 2*
 		break;
-	case 0x7A:
+	case 0x7A:	// NOP 2
 		break;
-	case 0x7B:
+	case 0x7B:	// RRA aby 7
 		break;
-	case 0x7C:
+	case 0x7C:	// NOP abx 4*
 		break;
-	case 0x7D:
+	case 0x7D:	// ADC abx 4*
 		break;
-	case 0x7E:
+	case 0x7E:	// ROR abx 7
 		break;
-	case 0x7F:
+	case 0x7F:	// RRA abx 7
 		break;
-	case 0x80:
+	case 0x80:	// NOP imm 2
 		break;
-	case 0x81:
+	case 0x81:	// STA izx 6
 		break;
-	case 0x82:
+	case 0x82:	// NOP imm 2
 		break;
-	case 0x83:
+	case 0x83:	// SAX izx 6
 		break;
-	case 0x84:
+	case 0x84:	// STY zp 3
 		break;
-	case 0x85:
+	case 0x85:	// STA zp 3
 		break;
-	case 0x86:
+	case 0x86:	// STX zp 3
 		break;
-	case 0x87:
+	case 0x87:	// SAX zp 3
 		break;
-	case 0x88:
+	case 0x88:	// DEY 2
 		break;
-	case 0x89:
+	case 0x89:	// NOP imm 2
 		break;
-	case 0x8A:
+	case 0x8A:	// TXA 2
 		break;
-	case 0x8B:
+	case 0x8B:	// XAA imm 2
 		break;
-	case 0x8C:
+	case 0x8C:	// STY abs 4
 		break;
-	case 0x8D:
+	case 0x8D:	// STA abs 4
 		break;
-	case 0x8E:
+	case 0x8E:	// STX abs 4
 		break;
-	case 0x8F:
+	case 0x8F:	// SAX abs 4
 		break;
-	case 0x90:
+	case 0x90:	// BCC rel 2*
 		break;
-	case 0x91:
+	case 0x91:	// STA izy 6
 		break;
-	case 0x92:
+	case 0x92:	// KIL
 		break;
-	case 0x93:
+	case 0x93:	// AHX izy 6
 		break;
-	case 0x94:
+	case 0x94:	// STY zpx 5
 		break;
-	case 0x95:
+	case 0x95:	// STA zpx 4
 		break;
-	case 0x96:
+	case 0x96:	// STX zpy 4
 		break;
-	case 0x97:
+	case 0x97:	// SAX zpy 4
 		break;
-	case 0x98:
+	case 0x98:	// TYA 2
 		break;
-	case 0x99:
+	case 0x99:	// STA aby 5
 		break;
-	case 0x9A:
+	case 0x9A:	// TXS 2
 		break;
-	case 0x9B:
+	case 0x9B:	// TAS aby 5
 		break;
-	case 0x9C:
+	case 0x9C:	// SHY abx 5
 		break;
-	case 0x9D:
+	case 0x9D:	// STA abx 5
 		break;
-	case 0x9E:
+	case 0x9E:	// SHX aby 5
 		break;
-	case 0x9F:
+	case 0x9F:	// AHX aby 5
 		break;
-	case 0xA0:
+	case 0xA0:	// LDY imm 2
 		break;
-	case 0xA1:
+	case 0xA1:	// LDA izx 6
 		break;
-	case 0xA2:
+	case 0xA2:	// LDX imm 2
 		break;
-	case 0xA3:
+	case 0xA3:	// LAX izx 6
 		break;
-	case 0xA4:
+	case 0xA4:	// LDY zp 3
 		break;
-	case 0xA5:
+	case 0xA5:	// LDA zp 3
 		break;
-	case 0xA6:
+	case 0xA6:	// LDX zp 3
 		break;
-	case 0xA7:
+	case 0xA7:	// LAX zp 3
 		break;
-	case 0xA8:
+	case 0xA8:	// TAY 2
 		break;
-	case 0xA9:
+	case 0xA9:	// LDA imm 2
 		break;
-	case 0xAA:
+	case 0xAA:	// TAX 2
 		break;
-	case 0xAB:
+	case 0xAB:	// LAX imm 2
 		break;
-	case 0xAC:
+	case 0xAC:	// LDY abs 4
 		break;
-	case 0xAD:
+	case 0xAD:	// LDA abs 4
 		break;
-	case 0xAE:
+	case 0xAE:	// LDX abs 4
 		break;
-	case 0xAF:
+	case 0xAF:	// LAX abs 4
 		break;
-	case 0xB0:
+	case 0xB0:	// BCS rel 2*
 		break;
-	case 0xB1:
+	case 0xB1:	// LDA izy 5*
 		break;
-	case 0xB2:
+	case 0xB2:	// KIL
 		break;
-	case 0xB3:
+	case 0xB3:	// LAX izy 5*
 		break;
-	case 0xB4:
+	case 0xB4:	// LDY zpx 4
 		break;
-	case 0xB5:
+	case 0xB5:	// LDA zpx 4
 		break;
-	case 0xB6:
+	case 0xB6:	// LDX zpy 4
 		break;
-	case 0xB7:
+	case 0xB7:	// LAX zpy 4
 		break;
-	case 0xB8:
+	case 0xB8:	// CLV 2
 		break;
-	case 0xB9:
+	case 0xB9:	// LDA aby 4*
 		break;
-	case 0xBA:
+	case 0xBA:	// TSX 2
 		break;
-	case 0xBB:
+	case 0xBB:	// LAS aby 4*
 		break;
-	case 0xBC:
+	case 0xBC:	// LDY abs 4*
 		break;
-	case 0xBD:
+	case 0xBD:	// LDA abx 4*
 		break;
-	case 0xBE:
+	case 0xBE:	// LDX aby 4*
 		break;
-	case 0xBF:
+	case 0xBF:	// LAX aby 4*
 		break;
 	case 0xC0:
 		break;
